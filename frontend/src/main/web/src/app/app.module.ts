d924db364a85de4a097860cf4b63a6cbe9f2ea4e
ajazgar <asia.jazgar@gmail.com>
2019-05-23 21:43:02 +0200

    airline logos

--- frontend/src/main/web/src/app/app.module.ts
+++ frontend/src/main/web/src/app/app.module.ts
@@ -11,7 +11,6 @@ import { ModalComponent } from './shared/modal/modal.component';
 import { RecommendationsComponent } from './shared/recommendations/recommendations.component';
 import { ResultTableComponent } from './shared/result-table/result-table.component';
 import { ReviewsComponent } from './shared/reviews/reviews.component';
-import { StepperComponent } from './shared/stepper/stepper.component';
 import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
 import { CarsComponent } from './pages/cars/cars.component';
 import { HotelsComponent } from './pages/hotels/hotels.component';
@@ -38,7 +37,6 @@ import { ArticleComponent } from './pages/article/article.component';
     RecommendationsComponent,
     ResultTableComponent,
     ReviewsComponent,
-    StepperComponent,
     NavBarComponent,
     CarsComponent,
     HotelsComponent,
