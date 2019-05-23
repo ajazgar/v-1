bf4f24f3bf215236d6ad0d7a81f8ff59bd6c496c
ajazgar <asia.jazgar@gmail.com>
2019-05-23 20:15:48 +0200

    articles done

--- frontend/src/main/web/src/app/pages/article/article.component.ts
+++ frontend/src/main/web/src/app/pages/article/article.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-article',
@@ -7,13 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArticleComponent implements OnInit {
 
+  articleTitle = [
+    {title: 'dia-de-muertos'},
+    {title: '5-kawiarni-amsterdam'},
+    {title: 'lodowce-islandia'}
+  ];
+
   amsterdamHeader = 'Top 5 kawiarni w Amsterdamie';
   amsterdamSec = 'Amsterdamskie kawiarnie to dużo więcej niż tylko smaczna kawa. ' +
     'To także porządna dawka holenderskiej kultury i sposobu życia. Od tradycyjnych Koffiehuis po przytulne ' +
     '"brązowe kafejki" - tutaj znajdziesz odpowiednie miejsce od napicia się kawy na każdą okazję. Sprawdź gdzie ' +
     'znaleźć najlepsze kawiarnie i bary w Amsterdamie!';
 
-  constructor() { }
+  mexicoHeader = 'Dia de muertos w Meksyku';
+  mexicoSec = 'Cukrowe czaszki, tańczące kościotrupy, kostucha w sombrero. ' +
+    'Meksykanie śmieją się śmierci w twarz, a śmierć uśmiecha się do nich. Zwłaszcza w czasie Día de Muertos.';
+
+  icelandHeader = 'Lodowce na Islandii';
+  icelandSec = 'Islandia jest znana jako “kraj ognia i lodu”. To określenie ma swoje źródło w wielu lodowcach i' +
+    ' wulkanach, które znajdują się na wyspie. Ile lodowców jest na Islandii? I czym właściwie jest lodowiec?';
+
+  constructor(private router: Router, private route: ActivatedRoute) {
+    this.route.params.subscribe( params => {
+      this.articleTitle = params['articleTitle'];
+    });
+  }
 
   ngOnInit() {
   }
